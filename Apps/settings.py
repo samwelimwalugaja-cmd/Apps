5611c8dec0e251456bd09eb743aa2c4aa7216c01
samwelimwalugaja-cmd <samwelimwalugaja@gmail.com>
2026-05-12 01:54:52 +0300

    fix static conflicts

--- Apps/settings.py
+++ Apps/settings.py
@@ -168,11 +168,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+
+STATICFILES_DIRS = [
+    BASE_DIR / "hycgWebsite" / "static",
+    BASE_DIR / "membership" / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_USE_FINDERS = True
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
